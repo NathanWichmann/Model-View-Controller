3c02db45265bb8a568686af0d2910845d8176dc9
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-05-24 16:51:05 -0400

    updates

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const path = require('path');
 const express = require('express');
-// Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require('./controllers/api/api1');
 const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -32,5 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
 });
+
